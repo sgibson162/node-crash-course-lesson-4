d64834e8d73e7e69acd8cea11a39d80cf3a67b4c
sgibson162 <148915771+sgibson162@users.noreply.github.com>
2023-11-24 15:21:12 -0500

    added lesson 7&8

--- node-crash-course-lesson-4/app.js
+++ node-crash-course-lesson-4/app.js
@@ -1,29 +1,38 @@
 const express = require('express');
+const morgan = require('morgan');
 
 // express app
 const app = express();
+ 
+
+// register view engine
+app.set('view engine', 'ejs');
 
 // listen for requests
 app.listen(3000);
 
-app.get('/', (req, res) {
+// middleware & static files
+app.use(express.static('public'));
+app.use(morgan('dev'));
 
-   // res.send('<p>home page</p>');
-    res.sendFile('./views/index.html', { root: __dirname });
+app.get('/', (req, res) => {
+    const blogs = [
+        {title: 'Yoshi finds eggs', snippet: 'hsdfkhskgnskfdh kshdfsj lsfj dsflksfj lijdf lsk'},
+        {title: 'Mario finds stars', snippet: 'hsdfkhskgnskfdh kshdfsj lsfj dsflksfj lijdf lsk'},
+        {title: 'How to defeat bowser', snippet: 'hsdfkhskgnskfdh kshdfsj lsfj dsflksfj lijdf lsk'},
+    ];
+    res.render('index', { title: 'home', blogs });
 });
 
-app.get('/about', (req, res) {
-
-    // res.send('<p>about page</p>');
-    res.sendFile('./views/about.html', { root: __dirname });
+app.get('/about', (req, res) => {
+    res.render('about', { title: 'About' });
 });
 
-// redirects
-app.get('/about-us', (req, res) => {
-    res.redirect('/about');
+app.get('/blogs/create', (req, res) => {
+    res.render('create', { title: 'Create a new Blog' });
 })
 
 // 404 page
 app.use((req, res) => {
-    res.status(404).sendFile('./views/404.html', { root: __dirname });    
+    res.status(404).render(404, { title: '404' });    
 });
