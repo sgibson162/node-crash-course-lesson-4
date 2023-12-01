55044b9b23d582e90b01503f5a8fb4bca10a15be
sgibson162 <148915771+sgibson162@users.noreply.github.com>
2023-12-01 14:42:29 -0500

    added lessons 9 & 10

--- node-crash-course-lesson-4/app.js
+++ node-crash-course-lesson-4/app.js
@@ -1,32 +1,83 @@
 const express = require('express');
 const morgan = require('morgan');
+const mongoose = require('mongoose')
+const Blog = require('./models/blog');
+
 
 // express app
 const app = express();
- 
+
+// connect to mongodb
+const dbURI = 'mongodb+srv://sgibson162:<Kick5@Hep8!>@node- tuts.dqbqn3x.mongodb.net/?retryWrites=true&w=majority' 
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true });
+    .then((result) => app.listen(3000))
+    .catch((err) => console.log(err));
 
 // register view engine
 app.set('view engine', 'ejs');
 
-// listen for requests
-app.listen(3000);
+
 
 // middleware & static files
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
 
+
+// routes
 app.get('/', (req, res) => {
-    const blogs = [
-        {title: 'Yoshi finds eggs', snippet: 'hsdfkhskgnskfdh kshdfsj lsfj dsflksfj lijdf lsk'},
-        {title: 'Mario finds stars', snippet: 'hsdfkhskgnskfdh kshdfsj lsfj dsflksfj lijdf lsk'},
-        {title: 'How to defeat bowser', snippet: 'hsdfkhskgnskfdh kshdfsj lsfj dsflksfj lijdf lsk'},
-    ];
-    res.render('index', { title: 'home', blogs });
+    res.redirect('/blogs');
 });
 
 app.get('/about', (req, res) => {
     res.render('about', { title: 'About' });
 });
+
+// blog routes
+app.get('/blogs', (req, res) => {
+     Blog.find().sort({ createdAt: -1 })
+     .then((result) => {
+        res.render('index', { title: 'All Blogs', blogs: result })
+     })
+     .catch((err) => {
+        console.log(err);
+     })
+});
+
+app.post('/blogs', (req, res) => {
+    const blog = Blog(req.body);
+
+    blog.save()
+    .then((result) => {
+        res.redirect('/blogs');
+    })
+    .catch((err) => {
+        console.log(err);
+    })
+})
+
+app.get('/blogs/:id', (req, res) => {
+    const id = req.params.id;
+    Blog.findById(id)
+    .then(result => {
+       res.render('details', { blog: result, title: 'Blog Details' }) 
+    })
+    .catch(err => {
+        console.log(err);
+    });
+})
+
+app.delete('/blogs/:id', (req, res) => {
+    const id = req.params.id;
+
+    Blog.findByIdAndDelete(id)
+    .then(result => {
+      res.json({ redirect: '/blogs' })  
+    })
+    .catch(err => {
+        console.log(err);
+    })
+})
 
 app.get('/blogs/create', (req, res) => {
     res.render('create', { title: 'Create a new Blog' });
